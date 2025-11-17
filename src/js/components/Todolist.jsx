af8a863bb56e74a825c2031f84948e4aa207f968
Pimple <araozlucas@gmail.com>
2025-11-17 22:25:46 +0000

    Third

--- src/js/components/Todolist.jsx
+++ src/js/components/Todolist.jsx
@@ -3,12 +3,16 @@ import { useState } from "react";
 
 export const Todolist = () => {
     
-    const [task, setTask] = useState ('');
-    const [toDos, setToDos] = useState ([   {id: 1, todo: 'tarea 1'},
+    const [ newTask, setNewTask ] = useState ('');
+    const [ editTask, setEditTask ] = useState('');
+    const [ editCompleted, setEditCompleted ] = useState('');
+
+    const [ toDos, setToDos] = useState ([   {id: 1, todo: 'tarea 1'},
                                             {id: 2, todo: 'tarea 2'},
-                                            {id: 3, todo: 'tarea 3'} ]);                                 
-    
-    const handleTask = (event) => {setTask(event.target.value)};
+                                            {id: 3, todo: 'tarea 3'} ]);                          
+    const [ editToDo, setEditToDo ] = useState ({});
+    const [ isEdit, setIsEdit ] = useState(false);
+    const handleNewTask = (event) => {setNewTask(event.target.value)};
 
     const handleDeleteTask = (deletedTask) => {
         setToDos(toDos.filter(item => item.id != deletedTask.id))
@@ -17,10 +21,10 @@ export const Todolist = () => {
     const handleSubmit = (event) => {
     // Tienes que agregar la tarea al array de toDos    
         event.preventDefault()
-        const dataToSend = {id: toDos.length + 1, todo : task};
-        if (task.trim() === '') return;
+        const dataToSend = {id: toDos.length + 1, todo : newTask};
+        if (newTask.trim() === '') return;
         setToDos([...toDos, dataToSend]);
-        setTask('');
+        setNewTask('');
         console.log(dataToSend);
     };
 
@@ -32,9 +36,9 @@ export const Todolist = () => {
                 {/* Form Agregar Tarea */}
                 <form onSubmit={handleSubmit}>
                     <div className="container text-start mb-3 p-3 bg-body-tertiary rounded-3">
-                        <label htmlFor="exampleInputTask" className="form-label text-primary"><small>Add task</small></label>
-                        <input type="text" className="form-control mb-2" id="exampleTask" aria-describedby="emailHelp" placeholder="New task" 
-                        value={task} onChange={handleTask}/>            
+                        <label htmlFor="exampleTask" className="form-label text-primary"><small>Add task</small></label>
+                        <input type="text" className="form-control mb-2" id="exampleTask" placeholder="New task" 
+                        value={newTask} onChange={handleNewTask}/>            
                     </div>
                 </form>
 
@@ -42,8 +46,8 @@ export const Todolist = () => {
                 <form >
                     <div className="container p-3 bg-body-tertiary rounded-3">
                         <div className="text-start mb-3">
-                            <label htmlFor="exampleInputTask" className="form-label text-primary"><small>Edit task</small></label>
-                            <input type="text" className="form-control mb-2" id="exampleTask" aria-describedby="emailHelp" placeholder="Pending"/>
+                            <label htmlFor="exampleInputPassword" className="form-label text-primary"><small>Edit task</small></label>
+                            <input type="text" className="form-control mb-2" id="exampleInputPassword" aria-describedby="emailHelp" placeholder="Pending"/>
                         </div>
                         <div className="text-start mb-3">
                             <input type="checkbox" className="form-check-input me-2" id="checkDefault" value=""/>
@@ -62,10 +66,17 @@ export const Todolist = () => {
                            {toDos.map((item, index) => {
                                 return (
                                 <li key={index} className="hidden-icon list-group-item d-flex justify-content-between">
-                                    {item.todo}
-                                    <span onClick={() => handleDeleteTask(item)}>
-                                        <i className="fa-solid fa-x text-danger"/>
-                                    </span>
+                                    <div>
+                                        <i className="fa-solid fa-check text-success me-1"></i>
+                                        <i className="fa-solid fa-x text-danger me-1"></i>
+                                        {item.todo}
+                                    </div>
+                                    <div>
+                                        <i class="fa-solid fa-pen-to-square text-primary me-1"></i>
+                                        <span onClick={() => handleDeleteTask(item)}>
+                                            <i className="fa-solid fa-trash text-danger"></i>
+                                        </span>
+                                    </div>
                                 </li>
                                 )
                             })}
